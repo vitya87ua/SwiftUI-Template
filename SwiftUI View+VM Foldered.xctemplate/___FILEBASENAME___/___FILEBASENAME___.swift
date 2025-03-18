95399b012817033e9f56ff2cf41e1b92e0980ec5
Viktor Berezhnytskyi <vberezhnytskyi@perpet.io>
2025-03-18 11:13:48 +0200

    Add: Foldered SwiftUI View+VM file

---
+++ SwiftUI View+VM Foldered.xctemplate/___FILEBASENAME___/___FILEBASENAME___.swift
@@ -0,0 +1,16 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+    
+    @StateObject var viewModel: ___FILEBASENAMEASIDENTIFIER___Model = ___FILEBASENAMEASIDENTIFIER___Model()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ___FILEBASENAMEASIDENTIFIER___()
+}
